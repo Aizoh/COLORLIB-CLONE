1ffb66266d8327c2d911387ecd4371ed086a1a7a
Aizoh <ghetaohmaina2@gmail.com>
2018-09-19 15:40:11 +0300

    modified files

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/danilonovakovic/pen/eLzqQr.
+A Pen created at CodePen.io. You can find this one at https://codepen.io/riogrande/pen/gbXxdx.
 
- Simple usable hamburger style navigation menu
+ Responsive flat animated overlay menu using css html and jquery nav navigation. There are two menus, classic and hamburger menu with full screen overlay. Its also flat. Links work when menu is downloaded. Icons from fontawesome 
--- js/index.js
+++ js/index.js
@@ -1,33 +1,11 @@
-const NAV_OPENED_CLASS = "fa-bars";
-const NAV_CLOSED_CLASS = "fa-times";
-
-window.onload = function() {
-  const navBtn = document.getElementById("nav-btn");
-  const classes = navBtn.classList;
-  const navbar = document.getElementById("navbar");
-
-  navBtn.addEventListener("click", function() {
-    if (classes.contains(NAV_OPENED_CLASS)) {
-      openNav();
-    } else {
-      closeNav();
-    }
-  });
-  const navLinks = document.getElementsByClassName("nav-link");
-  [...navLinks].forEach(function(elem) {
-    elem.addEventListener("click", function() {
-      closeNav();
+$(document).ready(function(){
+    $(".button a").click(function(){
+        $(".overlay").fadeToggle(200);
+       $(this).toggleClass('btn-open').toggleClass('btn-close');
     });
-  });
-
-  function closeNav() {
-    classes.remove(NAV_CLOSED_CLASS);
-    classes.add(NAV_OPENED_CLASS);
-    navbar.classList.add("hide");
-  }
-  function openNav() {
-    classes.remove(NAV_OPENED_CLASS);
-    classes.add(NAV_CLOSED_CLASS);
-    navbar.classList.remove("hide");
-  }
-};
+});
+$('.overlay').on('click', function(){
+    $(".overlay").fadeToggle(200);   
+    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+    open = false;
+});
