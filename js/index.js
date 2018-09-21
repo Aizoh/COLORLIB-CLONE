5f18b3b6647b5aab48fc037ffa45af90ad329e78
Aizoh <ghetaohmaina2@gmail.com>
2018-09-21 03:14:41 +0300

    added js animated counter feature on page load or reload

--- js/index.js
+++ js/index.js
@@ -9,3 +9,23 @@ $('.overlay').on('click', function(){
     $(".button a").toggleClass('btn-open').toggleClass('btn-close');
     open = false;
 });
+
+/*var i = 1;
+$(document.body).on('appear','.count',function() {
+    if (1 === i) trick();
+    i++;
+
+});*/
+
+//function trick(){ }
+ $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
